5c7fca433898ccd7728008d7f2b87e5294a7c57b
Rajkamal Kanna <rajkamalkanna4@gmail.com>
2023-07-13 11:58:03 +0000

    Display the “Where?” and “When?” options to be selected

--- src/session3-takehome/BookAppointment.js
+++ src/session3-takehome/BookAppointment.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BookAppointment() {
+  const [selectDoc, setSelectDoc] = useState("Select your doctor");
+
+  const handleChange = (event) => {
+    setSelectDoc(event.target.value);
+  };
   return (
     <>
       <h1>Book a session</h1>
@@ -21,24 +26,28 @@ function BookAppointment() {
         <input type="text" />
       </label>
       <h4>Doctor</h4>
-      <select>
-        <option selected>Select your doctor</option>
+      <select value={selectDoc} onChange={handleChange}>
+        <option>Select your doctor</option>
         <option>Dr. John Hopkin</option>
       </select>
       <br />
-      <h4>Where?</h4>
-      <div>
-        <input type="radio" id="google-meet" name="Google Meet" />
-        <label for="google-meet">Google Meet</label>
-      </div>
-      <div>
-        <input type="radio" id="phone" name="Phone" />
-        <label for="phone">Phone</label>
-      </div>
-      <h4>When?</h4>
-      <div>
-        <input type="datetime-local" id="date-details" name="date" />
-      </div>
+      {selectDoc == "Dr. John Hopkin" && (
+        <div>
+          <h4>Where?</h4>
+          <div>
+            <input type="radio" id="google-meet" name="action" />
+            <label for="google-meet">Google Meet</label>
+          </div>
+          <div>
+            <input type="radio" id="phone" name="action" />
+            <label for="phone">Phone</label>
+          </div>
+          <h4>When?</h4>
+          <div>
+            <input type="datetime-local" id="date-details" name="date" />
+          </div>
+        </div>
+      )}
       <button>Confirm Book</button>
     </>
   );
